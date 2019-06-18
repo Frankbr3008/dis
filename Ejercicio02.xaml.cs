4b2976c6df32577c10e2dfe20ebdf1d8f9b845b2
Frank Barrera <afrankbarrera@gmail.com>
2019-06-17 23:47:59 -0500

    unica version

---
+++ Ejercicio02.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Diseños
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Ejercicio02 : ContentPage
+    {
+        public Ejercicio02()
+        {
+            InitializeComponent();
+        }
+    }
+}
